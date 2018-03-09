cd968b946654c1b3cc81c8df2f45846ef23d6d8d
Zihao Chen <c361994752@gmail.com>
2018-03-09 17:10:59 +0800

    Add reference to restserver, restclient and logging_aux

--- hpcframework.py
+++ hpcframework.py
@@ -8,14 +8,15 @@ import logging
 import signal
 import sys
 import uuid
+import restserver
+import restclient
+import logging_aux
 
 from mesoshttp.client import MesosClient
 
+
 class Test(object):
-
-
     class MesosFramework(threading.Thread):
-
         def __init__(self, client):
             threading.Thread.__init__(self)
             self.client = client
@@ -27,15 +28,15 @@ class Test(object):
             except KeyboardInterrupt:
                 print('Stop requested by user, stopping framework....')
 
-
     def __init__(self):
         logging.basicConfig()
-        self.logger = logging.getLogger(__name__)
-        #signal.signal(signal.SIGINT, signal.SIG_IGN)
+        self.logger = logging_aux.init_logger_aux(
+            "hpcframework", "hpcframework.log")
+        # signal.signal(signal.SIGINT, signal.SIG_IGN)
         logging.getLogger('mesoshttp').setLevel(logging.DEBUG)
-        self.driver = None
+        self.driver = None  # type: MesosClient.SchedulerDriver
         self.client = MesosClient(mesos_urls=['http://172.16.1.4:5050'])
-        #self.client = MesosClient(mesos_urls=['zk://127.0.0.1:2181/mesos'])
+        # self.client = MesosClient(mesos_urls=['zk://127.0.0.1:2181/mesos'])
         self.client.on(MesosClient.SUBSCRIBED, self.subscribed)
         self.client.on(MesosClient.OFFERS, self.offer_received)
         self.client.on(MesosClient.UPDATE, self.status_update)
@@ -48,14 +49,12 @@ class Test(object):
                 self.shutdown()
                 break
 
-
     def shutdown(self):
         print('Stop requested by user, stopping framework....')
         self.logger.warn('Stop requested by user, stopping framework....')
         self.driver.tearDown()
         self.client.stop = True
         self.stop = True
-
 
     def subscribed(self, driver):
         self.logger.warn('SUBSCRIBED')
@@ -64,19 +63,20 @@ class Test(object):
     def status_update(self, update):
         # if update['status']['state'] == 'TASK_RUNNING':
         #     self.driver.kill(update['status']['agent_id']['value'], update['status']['task_id']['value'])
-        pass
+        print(str(update))
 
     def offer_received(self, offers):
-        # self.logger.warn('OFFER: %s' % (str(offers)))
+        # self.logger.info('OFFER: %s' % (str(offers)))
 
         for offer in offers:
-            print(str(offer.get_offer()))
+            self.logger.info("offer_received: {}".format(
+                (str(offer.get_offer()))))
             mesos_offer = offer.get_offer()
             if 'attributes' in mesos_offer:
                 attributes = mesos_offer['attributes']
                 if self.get_text(attributes, 'os') != 'windows_server':
-                    offer.decline()    
-                else:                    
+                    offer.decline()
+                else:
                     cores = self.get_scalar(attributes, 'cores')
                     cpus = self.get_scalar(mesos_offer['resources'], 'cpus')
 
@@ -86,10 +86,10 @@ class Test(object):
                         offer.decline()
             else:
                 offer.decline()
-                
 
     def accept_offer(self, offer):
-        print("Offer %s meets hpc's requiremnt" % offer.get_offer()['id']['value'])
+        self.logger.info("Offer %s meets hpc's requiremnt" %
+                         offer.get_offer()['id']['value'])
         self.run_job(offer)
 
         # i = 0
@@ -104,7 +104,7 @@ class Test(object):
             if i['name'] == name:
                 return i['scalar']['value']
         return 0.0
-    
+
     def get_text(self, dict, name):
         for i in dict:
             if i['name'] == name:
@@ -113,26 +113,33 @@ class Test(object):
 
     def run_job(self, mesos_offer):
         offer = mesos_offer.get_offer()
-        print(str(offer))
+        self.logger.info("Accepting offer: {}".format(str(offer)))
         task = {
             'name': 'sample test',
             'task_id': {'value': uuid.uuid4().hex},
             'agent_id': {'value': offer['agent_id']['value']},
             'resources': [
-            {
-                'name': 'cpus',
-                'type': 'SCALAR',
-                'scalar': {'value': 3.9}
-            },
-            {
-                'name': 'mem',
-                'type': 'SCALAR',
-                'scalar': {'value': 1000}
-            }
+                {
+                    'name': 'cpus',
+                    'type': 'SCALAR',
+                    'scalar': {'value': self.get_scalar(offer['resources'], 'cpus') - 0.1}
+                },
+                {
+                    'name': 'mem',
+                    'type': 'SCALAR',
+                    'scalar': {'value': self.get_scalar(offer['resources'], 'mem')}
+                }
             ],
-            'command': {'value': 'powershell c:\\HPCPack2016\\5.1.6086.0\\setupscript.ps1'}            
+            'command': {'value': 'powershell sleep 3000'}
         }
 
         mesos_offer.accept([task])
 
-test_mesos = Test()
+
+if __name__ == "__main__":
+    rest_server = restserver.RestServer(8088)
+    server_thread = threading.Thread(target=rest_server.run)
+    server_thread.start()
+    test_mesos = Test()
+    rest_server.stop()
+    server_thread.join()
