df8de520bcc32ec2c06792045c4ae7b9122f34f5
Zihao Chen <c361994752@gmail.com>
2018-03-12 14:30:23 +0800

    Load and concat setup script to command

--- hpcframework.py
+++ hpcframework.py
@@ -8,12 +8,16 @@ import logging
 import signal
 import sys
 import uuid
+import base64
+import codecs
 import restserver
 import restclient
 import logging_aux
 
 from mesoshttp.client import MesosClient
+from restclient import AutoScaleRestClient
 
+from mesoshttp.offers import Offer
 
 class Test(object):
     class MesosFramework(threading.Thread):
@@ -34,13 +38,24 @@ class Test(object):
             "hpcframework", "hpcframework.log")
         # signal.signal(signal.SIGINT, signal.SIG_IGN)
         logging.getLogger('mesoshttp').setLevel(logging.DEBUG)
+
+        self.hpc_client = AutoScaleRestClient()   
+        self.core_provisioning = 0.0     
+        with open("setupscript.ps1") as scriptfile:
+            hpc_setup_ps1 = scriptfile.read()
+        self.logger.info("Loaded HPC setup script:/n{}".format(hpc_setup_ps1))
+        hpc_setup_ps1_utf16 = hpc_setup_ps1.encode('utf-16')
+        hpc_setup_ps1_utf16_nobom = hpc_setup_ps1_utf16[2:] if hpc_setup_ps1_utf16[0:2] == codecs.BOM_UTF16 else hpc_setup_ps1_utf16
+        self.hpc_setup_ps1_b64 = base64.b64encode(hpc_setup_ps1_utf16_nobom)
+        
+
         self.driver = None  # type: MesosClient.SchedulerDriver
-        self.client = MesosClient(mesos_urls=['http://172.16.1.4:5050'])
+        self.mesos_client = MesosClient(mesos_urls=['http://172.16.1.4:5050'])
         # self.client = MesosClient(mesos_urls=['zk://127.0.0.1:2181/mesos'])
-        self.client.on(MesosClient.SUBSCRIBED, self.subscribed)
-        self.client.on(MesosClient.OFFERS, self.offer_received)
-        self.client.on(MesosClient.UPDATE, self.status_update)
-        self.th = Test.MesosFramework(self.client)
+        self.mesos_client.on(MesosClient.SUBSCRIBED, self.subscribed)
+        self.mesos_client.on(MesosClient.OFFERS, self.offer_received)
+        self.mesos_client.on(MesosClient.UPDATE, self.status_update)
+        self.th = Test.MesosFramework(self.mesos_client)
         self.th.start()
         while True and self.th.isAlive():
             try:
@@ -53,7 +68,7 @@ class Test(object):
         print('Stop requested by user, stopping framework....')
         self.logger.warn('Stop requested by user, stopping framework....')
         self.driver.tearDown()
-        self.client.stop = True
+        self.mesos_client.stop = True
         self.stop = True
 
     def subscribed(self, driver):
@@ -67,25 +82,30 @@ class Test(object):
 
     def offer_received(self, offers):
         # self.logger.info('OFFER: %s' % (str(offers)))
-
-        for offer in offers:
-            self.logger.info("offer_received: {}".format(
-                (str(offer.get_offer()))))
-            mesos_offer = offer.get_offer()
-            if 'attributes' in mesos_offer:
-                attributes = mesos_offer['attributes']
-                if self.get_text(attributes, 'os') != 'windows_server':
-                    offer.decline()
-                else:
-                    cores = self.get_scalar(attributes, 'cores')
-                    cpus = self.get_scalar(mesos_offer['resources'], 'cpus')
-
-                    if cores == cpus:
-                        self.accept_offer(offer)
-                    else:
+        grow_decision = self.hpc_client.get_grow_decision()
+        
+        if(grow_decision.cores_to_grow - self.core_provisioning > 0):
+            for offer in offers: # type: Offer                
+                self.logger.info("offer_received: {}".format(
+                    (str(offer.get_offer()))))
+                mesos_offer = offer.get_offer()
+                if 'attributes' in mesos_offer:
+                    attributes = mesos_offer['attributes']
+                    if self.get_text(attributes, 'os') != 'windows_server':
                         offer.decline()
-            else:
-                offer.decline()
+                    else:
+                        cores = self.get_scalar(attributes, 'cores')
+                        cpus = self.get_scalar(mesos_offer['resources'], 'cpus')
+
+                        if cores == cpus:
+                            self.accept_offer(offer)
+                        else:
+                            offer.decline()
+                else:
+                    offer.decline()
+        else:
+            for offer in offers:
+                offer.decline()                
 
     def accept_offer(self, offer):
         self.logger.info("Offer %s meets hpc's requiremnt" %
@@ -130,9 +150,9 @@ class Test(object):
                     'scalar': {'value': self.get_scalar(offer['resources'], 'mem')}
                 }
             ],
-            'command': {'value': 'powershell sleep 3000'}
+            'command': {'value': 'powershell -EncodedCommand ' + self.hpc_setup_ps1_b64}
         }
-
+        self.logger.debug("Sending command:/n{}".format(task['command']['value']))
         mesos_offer.accept([task])
 
 
