3d049d52fcfa45e3cc7cb1b85cda3f9c90f4b2a6
Zihao Chen <c361994752@gmail.com>
2018-06-06 07:27:27 +0000

    Add take_node_offline and assign_node_template rest call

--- restclient.py
+++ restclient.py
@@ -12,18 +12,21 @@ IdleNode = namedtuple("IdleNode", "node_name timestamp server_name")
 
 class HpcRestClient(object):
     DEFAULT_NODEGROUP_TOKEN = "default"
+    DEFAULT_COMPUTENODE_TEMPLATE = "Default ComputeNode Template"
     # auto-scale api set
     GROW_DECISION_API_ROUTE = "https://{}/HpcManager/api/auto-scale/grow-decision"
     CHECK_NODES_IDLE_ROUTE = "https://{}/HpcManager/api/auto-scale/check-nodes-idle"
     # node management api set
     BRING_NODE_ONLINE_ROUTE = "https://{}/HpcManager/api/nodes/bringOnline"
+    TAKE_NODE_OFFLINE_ROUTE = "https://{}/HpcManager/api/nodes/takeOffline"
+    ASSIGN_NODE_TEMPLATE_ROUTE = "https://{}/HpcManager/api/nodes/assignTemplate"
 
     def __init__(self, hostname="localhost"):
         self.hostname = hostname
         self.logger = logging_aux.init_logger_aux("hpcframework.restclient", 'hpcframework.restclient.log')
 
     def _log_error(self, function_name, res):
-            self.logger.error("{}: status_code:{} content:{}".format(function_name, res.status_code, res.content))       
+        self.logger.error("{}: status_code:{} content:{}".format(function_name, res.status_code, res.content))
 
     def _log_info(self, function_name, res):
         self.logger.info(function_name + ":" + res.content)
@@ -39,6 +42,7 @@ class HpcRestClient(object):
             self._log_error(function_name, res)
             return False, None
 
+    # Starts auto-scale api
     def get_grow_decision(self):
         url = self.GROW_DECISION_API_ROUTE.format(self.hostname)
         res = requests.post(url, verify=False)
@@ -50,22 +54,37 @@ class HpcRestClient(object):
             self.logger.error("status_code:{} content:{}".format(res.status_code, res.content))
 
     def check_nodes_idle(self, nodes):
-        success, res = self._post(self.check_nodes_idle.__name__, self.CHECK_NODES_IDLE_ROUTE, nodes)        
-        if success:            
+        success, res = self._post(self.check_nodes_idle.__name__, self.CHECK_NODES_IDLE_ROUTE, nodes)
+        if success:
             jobjs = json.loads(res.content)
             return [IdleNode(idle_info['NodeName'], idle_info['TimeStamp'], idle_info['ServerName']) for idle_info in jobjs]
-    
 
+    # Starts node management api
     def bring_node_online(self, nodes):
         success, res = self._post(self.bring_node_online.__name__, self.BRING_NODE_ONLINE_ROUTE, nodes)
         if success:
             jobj = json.loads(res.content)
             return jobj
 
+    def take_node_offline(self, nodes):
+        success, res = self._post(self.take_node_offline.__name__, self.TAKE_NODE_OFFLINE_ROUTE, nodes)
+        if success:
+            jobj = json.loads(res.content)
+            return jobj
+
+    def assign_node_template(self, nodename_arr, template_name):
+        params = json.dumps({"nodeNames": nodename_arr, "templateName": template_name})
+        success, res = self._post(self.assign_node_template.__name__, self.ASSIGN_NODE_TEMPLATE_ROUTE, params)
+        if success:
+            jobj = json.loads(res.content)
+            return jobj
+
+
 if __name__ == '__main__':
     client = HpcRestClient()
     ans = client.get_grow_decision()
     print ans.cores_to_grow
     print client.check_nodes_idle(json.dumps(['mesoswinjd']))
-    
+
     print client.bring_node_online(json.dumps(['mesoswinjd']))
+    print client.assign_node_template(['iaascn000'], client.DEFAULT_COMPUTENODE_TEMPLATE)
