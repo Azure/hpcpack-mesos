ed02b66e06c85c7e6a90a5b458e3b77b0c0ac960
Zihao Chen <c361994752@gmail.com>
2018-06-15 13:46:51 +0800

    Check idle time out using datetime math

--- heartbeat_table.py
+++ heartbeat_table.py
@@ -7,24 +7,25 @@ import logging_aux
 
 class HpcClusterManager(object):
 
-    CHECK_CONFIGURING_NODES_INTERVAL = 5 # in seconds
+    CHECK_CONFIGURING_NODES_INTERVAL = 5  # in seconds
     MESOS_NODE_GROUP_NAME = "Mesos"
     MESOS_NODE_GROUP_DESCRIPTION = "The Mesos compute nodes in the cluster"
 
-    CHECK_IDLE_INTERVAL = 60.0
     NODE_IDLE_TIMEOUT = 180.0
 
     def __init__(self, hpc_rest_client, provisioning_timeout=timedelta(minutes=15), heartbeat_timeout=timedelta(minutes=3)):
         # type: (HpcRestClient, timedelta, timedelta) -> ()
         self._heart_beat_table = {}
-        self.node_idle_check_table = {}
-        self.logger = logging_aux.init_logger_aux("hpcframework.clustermanager", "hpcframework.clustermanager.log")        
+        self._node_idle_check_table = {}
+        self.logger = logging_aux.init_logger_aux("hpcframework.clustermanager", "hpcframework.clustermanager.log")
         self._table_lock = threading.Lock()
         self._provisioning_timeout = provisioning_timeout
         self._heartbeat_timeout = heartbeat_timeout
         self._hpc_client = hpc_rest_client
 
-        # callbacks       
+        self._node_idle_timedelta = timedelta(0, self.NODE_IDLE_TIMEOUT)
+
+        # callbacks
         self._node_closed_callbacks = []
 
     def __get_hostname_from_fqdn(self, fqdn):
@@ -33,6 +34,7 @@ class HpcClusterManager(object):
     '''
     callback has signature List[str] -> ()
     '''
+
     def subscribe_node_closed_callback(self, callback):
         self._node_closed_callbacks.append(callback)
 
@@ -45,7 +47,8 @@ class HpcClusterManager(object):
                     hostname, self._heart_beat_table[hostname].fqdn, u_fqdn))
                 return
             elif self._heart_beat_table[hostname].state != HpcState.Closed:
-                self.logger.warn("Heart beat entry of {} existed. old value: {}.".format(hostname, str(self._heart_beat_table[hostname])))
+                self.logger.warn("Heart beat entry of {} existed. old value: {}.".format(
+                    hostname, str(self._heart_beat_table[hostname])))
         slaveinfo = SlaveInfo(hostname, u_fqdn, agent_id, task_id, cpus, last_heartbeat, HpcState.Provisioning)
         self._heart_beat_table[hostname] = slaveinfo
         self.logger.info("Heart beat entry added: {}".format(str(slaveinfo)))
@@ -58,7 +61,8 @@ class HpcClusterManager(object):
             if self._heart_beat_table[u_hostname].state == HpcState.Provisioning:
                 with self._table_lock:
                     if self._heart_beat_table[u_hostname].state == HpcState.Provisioning:
-                        self._heart_beat_table[u_hostname] = self._heart_beat_table[u_hostname]._replace(state=HpcState.Configuring)
+                        self._heart_beat_table[u_hostname] = self._heart_beat_table[u_hostname]._replace(
+                            state=HpcState.Configuring)
                         self.logger.info("Configuring Host {}".format(u_hostname))
         else:
             self.logger.error("Host {} is not recognized. Heartbeat ignored.".format(u_hostname))
@@ -113,7 +117,7 @@ class HpcClusterManager(object):
                 else:
                     running_list.append(host)
         return (provision_timeout_list, heartbeat_timeout_list, running_list)
-    
+
     def get_cores_in_provisioning(self):
         cores = 0.0
         for host in dict(self._heart_beat_table).itervalues():
@@ -127,13 +131,13 @@ class HpcClusterManager(object):
         configured_node_names = []
         for host in dict(self._heart_beat_table).itervalues():
             if host.state == HpcState.Configuring:
-                configuring_node_names.append(host.hostname)     
+                configuring_node_names.append(host.hostname)
         if configuring_node_names:
-            self.logger.info("Nodes in configuring: {}".format(configuring_node_names))              
-            configured_node_names = self._configure_compute_nodes_state_machine(configuring_node_names)            
+            self.logger.info("Nodes in configuring: {}".format(configuring_node_names))
+            configured_node_names = self._configure_compute_nodes_state_machine(configuring_node_names)
         if configured_node_names:
-            self.logger.info("Nodes configured: {}".format(configured_node_names)) 
-            self._set_nodes_running(configured_node_names)            
+            self.logger.info("Nodes configured: {}".format(configured_node_names))
+            self._set_nodes_running(configured_node_names)
 
     def _configure_compute_nodes_state_machine(self, configuring_node_names):
         if not configuring_node_names:
@@ -157,16 +161,16 @@ class HpcClusterManager(object):
             if node_status[HpcRestClient.NODE_STATUS_NODE_HEALTH_KEY] == HpcRestClient.NODE_STATUS_NODE_HEALTH_UNAPPROVED_VALUE:
                 unapproved_node_list.append(node_name)
             # node approved
-            elif self.MESOS_NODE_GROUP_NAME not in node_status[HpcRestClient.NODE_STATUS_NODE_GROUP_KEY]:                
+            elif self.MESOS_NODE_GROUP_NAME not in node_status[HpcRestClient.NODE_STATUS_NODE_GROUP_KEY]:
                 if node_state == HpcRestClient.NODE_STATUS_NODE_STATE_ONLINE_VALUE:
-                    take_offline_node_list.append(node_name)                
+                    take_offline_node_list.append(node_name)
                 elif node_state == HpcRestClient.NODE_STATUS_NODE_STATE_OFFLINE_VALUE:
                     change_node_group_node_list.append(node_name)
                 else:
                     invalid_state_node_dict[node_name] = node_state
             # node group properly set
             elif node_state == HpcRestClient.NODE_STATUS_NODE_STATE_OFFLINE_VALUE:
-                bring_online_node_list.append(node_name)   
+                bring_online_node_list.append(node_name)
             elif node_state == HpcRestClient.NODE_STATUS_NODE_STATE_ONLINE_VALUE:
                 # this node is all set
                 configured_node_names.append(node_name)
@@ -174,27 +178,27 @@ class HpcClusterManager(object):
                 invalid_state_node_dict[node_name] = node_state
         try:
             if invalid_state_node_dict:
-                self.logger.info("Node(s) in invalid state when configuring: {}".format(str(invalid_state_node_dict)))   
+                self.logger.info("Node(s) in invalid state when configuring: {}".format(str(invalid_state_node_dict)))
             if unapproved_node_list:
-                self.logger.info("Assigning node template for node(s): {}".format(str(unapproved_node_list)))   
+                self.logger.info("Assigning node template for node(s): {}".format(str(unapproved_node_list)))
                 self._hpc_client.assign_default_compute_node_template(unapproved_node_list)
             if take_offline_node_list:
-                self.logger.info("Taking node(s) offline: {}".format(str(take_offline_node_list)))   
+                self.logger.info("Taking node(s) offline: {}".format(str(take_offline_node_list)))
                 self._hpc_client.take_nodes_offline(take_offline_node_list)
             if bring_online_node_list:
-                self.logger.info("Bringing node(s) online: {}".format(str(bring_online_node_list)))   
+                self.logger.info("Bringing node(s) online: {}".format(str(bring_online_node_list)))
                 self._hpc_client.bring_nodes_online(bring_online_node_list)
             if change_node_group_node_list:
-                self.logger.info("Changing node group node(s): {}".format(str(change_node_group_node_list)))   
+                self.logger.info("Changing node group node(s): {}".format(str(change_node_group_node_list)))
                 self._hpc_client.add_node_to_node_group(self.MESOS_NODE_GROUP_NAME, change_node_group_node_list)
         except:
             # Swallow all exceptions here. As we don't want any exception to prevent configured nodes to work
-            self.logger.exception('Exception happened when configuring compute node.')                        
-        
+            self.logger.exception('Exception happened when configuring compute node.')
+
         return configured_node_names
 
     def _check_runaway_and_idle_compute_nodes(self):
-        (provision_timeout_list, heartbeat_timeout_list, running_list) = self.check_timeout()        
+        (provision_timeout_list, heartbeat_timeout_list, running_list) = self.check_timeout()
         if provision_timeout_list:
             self.logger.info("Get provision_timeout_list:{}".format(str(provision_timeout_list)))
             self._set_nodes_draining(host.hostname for host in provision_timeout_list)
@@ -211,14 +215,14 @@ class HpcClusterManager(object):
 
     def _check_node_idle_timeout(self, node_names):
         new_node_idle_check_table = {}
-        max_tolerance = self.NODE_IDLE_TIMEOUT / self.CHECK_IDLE_INTERVAL
         for node_name in node_names:
-            if node_name in self.node_idle_check_table:
-                new_node_idle_check_table[node_name] = self.node_idle_check_table[node_name] + 1
+            if node_name in self._node_idle_check_table:
+                new_node_idle_check_table[node_name] = self._node_idle_check_table[node_name]
             else:
-                new_node_idle_check_table[node_name] = 1
-        self.node_idle_check_table = new_node_idle_check_table
-        return [name for name, value in self.node_idle_check_table.iteritems() if value > max_tolerance]
+                new_node_idle_check_table[node_name] = datetime.now()
+        self._node_idle_check_table = new_node_idle_check_table
+        now = datetime.now()
+        return [name for name, value in self._node_idle_check_table.iteritems() if (now - value) > self._node_idle_timedelta]
 
     def start_configure_cluster_timer(self):
         self._configure_compute_nodes()
@@ -229,9 +233,9 @@ class HpcClusterManager(object):
         timer.start()
 
     def start(self):
-        self.start_configure_cluster_timer()   
+        self.start_configure_cluster_timer()
 
-    def _set_nodes_draining(self, node_names):        
+    def _set_nodes_draining(self, node_names):
         # type: (List[str]) -> ()
         self._set_node_state(node_names, HpcState.Draining, "Draining")
 
@@ -240,14 +244,14 @@ class HpcClusterManager(object):
         self._set_node_state(node_names, HpcState.Closing, "Closing")
 
     def _set_nodes_closed(self, node_names):
-        # type: (List[str]) -> ()        
+        # type: (List[str]) -> ()
         closed_nodes = self._set_node_state(node_names, HpcState.Closed, "Closed")
         if self._node_closed_callbacks:
             for callback in self._node_closed_callbacks:
                 callback(closed_nodes)
 
     def _set_nodes_running(self, node_names):
-        # type: (List[str]) -> ()        
+        # type: (List[str]) -> ()
         self._set_node_state(node_names, HpcState.Running, "Running")
 
     def _set_node_state(self, node_names, node_state, state_name):
@@ -260,7 +264,8 @@ class HpcClusterManager(object):
                     old_state = self._heart_beat_table[u_hostname].state
                     self._heart_beat_table[u_hostname] = self._heart_beat_table[u_hostname]._replace(state=node_state)
                     setted_nodes.append(u_hostname)
-                    self.logger.info("Host {} set to {} from {}.".format(u_hostname, state_name, HpcState.Names[old_state]))
+                    self.logger.info("Host {} set to {} from {}.".format(
+                        u_hostname, state_name, HpcState.Names[old_state]))
             else:
                 self.logger.error("Host {} is not recognized. State {} Ignored.".format(u_hostname, state_name))
         return setted_nodes
@@ -275,12 +280,12 @@ class HpcClusterManager(object):
                 node_names_to_close.append(host.hostname)
 
         if node_names_to_drain:
-            drained_node_names = self._drain_nodes_state_machine(node_names_to_drain)        
+            drained_node_names = self._drain_nodes_state_machine(node_names_to_drain)
             if drained_node_names:
                 self.logger.info("Drained nodes:{}".format(drained_node_names))
                 self._set_nodes_closing(drained_node_names)
-                node_names_to_close += drained_node_names # short-cut the drained nodes to close
-        
+                node_names_to_close += drained_node_names  # short-cut the drained nodes to close
+
         if node_names_to_close:
             closed_node_names, re_drain_node_names = self._close_node_state_machine(node_names_to_close)
             if closed_node_names:
@@ -309,13 +314,13 @@ class HpcClusterManager(object):
                 invalid_state_node_dict[node_name] = node_state
         try:
             if invalid_state_node_dict:
-                self.logger.info("Node(s) in invalid state when draining: {}".format(str(invalid_state_node_dict)))   
+                self.logger.info("Node(s) in invalid state when draining: {}".format(str(invalid_state_node_dict)))
             if take_offline_node_list:
-                self.logger.info("Taking node(s) offline: {}".format(str(take_offline_node_list)))   
+                self.logger.info("Taking node(s) offline: {}".format(str(take_offline_node_list)))
                 self._hpc_client.take_nodes_offline(take_offline_node_list)
         except:
             # Swallow all exceptions here. As we don't want any exception to prevent drained nodes to be closed
-            self.logger.exception('Exception happened when draining compute node.')        
+            self.logger.exception('Exception happened when draining compute node.')
 
         return drained_node_names
 
@@ -344,13 +349,14 @@ class HpcClusterManager(object):
 
         try:
             if to_remove_node_names:
-                self.logger.info("Remove node(s): {}".format(to_remove_node_names))   
+                self.logger.info("Remove node(s): {}".format(to_remove_node_names))
                 self._hpc_client.remove_nodes(to_remove_node_names)
         except:
             # Swallow all exceptions here. As we don't want any exception to prevent removed nodes go to closed
-            self.logger.exception('Exception happened when configuring compute node.')  
+            self.logger.exception('Exception happened when configuring compute node.')
 
-        return closed_node, re_drain_node_names     
+        return closed_node, re_drain_node_names
+
 
 SlaveInfo = namedtuple("SlaveInfo", "hostname fqdn agent_id task_id cpus last_heartbeat state")
 
