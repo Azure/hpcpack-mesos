e95cef9a365f91c253d975da0cccc7b7587dea4a
Zihao Chen <c361994752@gmail.com>
2018-06-27 17:00:50 +0800

    Code cleanup and warning fix in hpcframework

--- hpcframework.py
+++ hpcframework.py
@@ -13,6 +13,20 @@ import restserver
 from restclient import HpcRestClient
 
 
+def get_text(collection, name):
+    for i in collection:
+        if i['name'] == name:
+            return i['text']['value']
+    return ""
+
+
+def get_scalar(collection, name):
+    for i in collection:
+        if i['name'] == name:
+            return i['scalar']['value']
+    return 0.0
+
+
 class HpcpackFramwork(object):
     class MesosFramework(threading.Thread):
         def __init__(self, client):
@@ -38,38 +52,25 @@ class HpcpackFramwork(object):
         self.ssl_thumbprint = ssl_thumbprint
         self.framework_uri = framework_uri
         self.node_group = node_group
-        self.hpc_client = HpcRestClient()  # TODO: can we make hpc_client temp var?
-
+        self.hpc_client = HpcRestClient()
         self.heartbeat_table = heartbeat_table.HpcClusterManager(self.hpc_client, node_group=self.node_group)
         self.heartbeat_table.subscribe_node_closed_callback(lambda l: map(self._kill_task_by_hostname, l))
         self.heartbeat_table.start()
-
         self.core_provisioning = 0.0
-
-        '''
-        with open("setupscript.ps1") as scriptfile:
-            hpc_setup_ps1 = scriptfile.read()
-        with open("daemon.ps1") as daemonFile:
-            hpc_daemon_ps1 = daemonFile.read()
-        self.logger.debug("Loaded HPC daemon script:\n{}".format(hpc_setup_ps1))
-        hpc_daemon_ps1_b64 = self.__encode_utf16b64(hpc_daemon_ps1)
-        hpc_setup_fromed = hpc_setup_ps1.replace("$$encodedDaemonScript", '"' + hpc_daemon_ps1_b64 + '"')
-        self.logger.debug("Loaded and formed HPC setup script:\n{}".format(hpc_setup_fromed))
-        self.hpc_setup_ps1_b64 = self.__encode_utf16b64(hpc_setup_fromed)
-        '''
-
         self.driver = None  # type: MesosClient.SchedulerDriver
         framework_suffix = self.headnode.replace(',', '_')
         if self.node_group != "":
             framework_suffix = framework_suffix + '-' + self.node_group
         self.mesos_client = MesosClient(mesos_urls=['http://172.16.1.4:5050'],
+                                        # mesos_urls=['zk://127.0.0.1:2181/mesos'],
                                         frameworkName="HPC-Pack-Framework-{}".format(framework_suffix))
-        # self.mesos_client = MesosClient(mesos_urls=['zk://127.0.0.1:2181/mesos'])
+
         self.mesos_client.on(MesosClient.SUBSCRIBED, self.subscribed)
         self.mesos_client.on(MesosClient.OFFERS, self.offer_received)
         self.mesos_client.on(MesosClient.UPDATE, self.status_update)
         self.th = HpcpackFramwork.MesosFramework(self.mesos_client)
         self.heartbeat_server = restserver.HeartBeatServer(self.heartbeat_table, 8088)
+        self.stop = False
 
     def start(self):
         self.th.start()
@@ -128,18 +129,18 @@ class HpcpackFramwork(object):
                         cores_to_grow, cores_in_provisioning, (str(offer_dict))))
                     if 'attributes' in offer_dict:
                         attributes = offer_dict['attributes']
-                        if self.get_text(attributes, 'os') == 'windows_server':
+                        if get_text(attributes, 'os') == 'windows_server':
                             match_node_group = False
                             if self.node_group == "":
                                 match_node_group = True
-                            elif self.get_text(attributes, 'node_group').upper() == self.node_group.upper():
+                            elif get_text(attributes, 'node_group').upper() == self.node_group.upper():
                                 match_node_group = True
                             else:
                                 match_node_group = False
 
                             if match_node_group:
-                                cores = self.get_scalar(attributes, 'cores')
-                                cpus = self.get_scalar(offer_dict['resources'], 'cpus')
+                                cores = get_scalar(attributes, 'cores')
+                                cpus = get_scalar(offer_dict['resources'], 'cpus')
                                 if cores == cpus:
                                     if not self.heartbeat_table.check_fqdn_collision(offer_dict['hostname']):
                                         take_offer = True
@@ -154,32 +155,18 @@ class HpcpackFramwork(object):
         except Exception as ex:
             self.logger.exception(ex)
 
-    def accept_offer(self, offer):
-        self.logger.info("Offer %s meets HPC's requirement" % offer.get_offer()['id']['value'])
-        self.run_job(offer)
-
     def decline_offer(self, offer):
+        self.logger.info("Decline offer %s" % offer.get_offer()['id']['value'])
         offer.decline()
 
-    def get_scalar(self, collection, name):
-        for i in collection:
-            if i['name'] == name:
-                return i['scalar']['value']
-        return 0.0
-
-    def get_text(self, collection, name):
-        for i in collection:
-            if i['name'] == name:
-                return i['text']['value']
-        return ""
-
-    def run_job(self, mesos_offer):
-        offer_dict = mesos_offer.get_offer()
+    def accept_offer(self, offer):
+        self.logger.info("Offer %s meets HPC's requirement" % offer.get_offer()['id']['value'])
+        offer_dict = offer.get_offer()
         self.logger.info("Accepting offer: {}".format(str(offer_dict)))
         agent_id = offer_dict['agent_id']['value']
         fqdn = offer_dict['hostname']
         task_id = uuid.uuid4().hex
-        cpus = self.get_scalar(offer_dict['resources'], 'cpus')
+        cpus = get_scalar(offer_dict['resources'], 'cpus')
 
         task = {
             'name': 'hpc pack mesos cn',
@@ -195,7 +182,7 @@ class HpcpackFramwork(object):
                 {
                     'name': 'mem',
                     'type': 'SCALAR',
-                    'scalar': {'value': self.get_scalar(offer_dict['resources'], 'mem')}
+                    'scalar': {'value': get_scalar(offer_dict['resources'], 'mem')}
                 }
             ],
             'command': {'value':
@@ -203,7 +190,7 @@ class HpcpackFramwork(object):
                             " -headnode " + self.headnode + " -sslthumbprint " + self.ssl_thumbprint + " -frameworkUri " + self.framework_uri + " > setupscript.log"}
         }
         self.logger.debug("Sending command:\n{}".format(task['command']['value']))
-        mesos_offer.accept([task])
+        offer.accept([task])
         self.heartbeat_table.add_slaveinfo(fqdn, agent_id, task_id, cpus)
 
     def _kill_task(self, host):
